f4671b51910d9ea2f84e17941c8815104e9ceec6
SHEHAN <92236288+Shehan17@users.noreply.github.com>
2026-01-03 00:51:49 +0530

    Add user registration and appointment booking modules . (#8)
    Co-authored-by: G.S. Sandeepa <sandeepagulawita.42@gmail.com>

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,15 +1,25 @@
+using MedUnity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.ComponentModel.DataAnnotations;
+using MedUnity.Models;
 
 namespace MedUnity.Pages
 {
     public class LoginModel : PageModel
     {
+        private readonly AppDbContext _context;
+
+        public LoginModel(AppDbContext context)
+        {
+            _context = context; 
+        }
+
 
         [BindProperty, Required]
         public string FirstName { get; set; } = string.Empty;
-
+    
         [BindProperty, Required]
         public string LastName { get; set; } = string.Empty;
 
@@ -30,6 +40,32 @@ namespace MedUnity.Pages
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var login = new Patient
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                // DateOfBirth =
+                Email = Email,
+                PasswordHash = password,
+                PhoneNumber = PhoneNumber,
+                SpecialNote = SpecialNote,
+
+            };
+
+            _context.Patients.Add(login);
+            _context.SaveChanges();
+
+            return RedirectToPage("Index");
+        }
+
 
 
 
